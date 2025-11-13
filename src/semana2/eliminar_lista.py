4ba121beed290037204568d96c7937fc85e86a7f
Brayan <brayacamacho5@gmail.com>
2025-11-13 22:07:12 +0100

    aprendiendo a eliminar listas

---
+++ src/semana2/eliminar_lista.py
@@ -0,0 +1,25 @@
+# Lista inicial
+frutas = ["manzana", "pera", "kiwi"]
+
+print(f"Las frutas son: {frutas}")
+
+# Cambiar una fruta
+posicion = int(input("Que posicion quieres cambiar 0, 1 , 2: "))
+nueva_fruta = input("Cual es la fruta nueva ")
+
+frutas[posicion] = nueva_fruta
+
+print("Frutas actualizadas", frutas)
+
+# Eliminar una fruta
+while True:
+    try:
+        eliminar = input("Que fruta deseas eliminar: ")
+        if eliminar in frutas:
+            frutas.remove(eliminar)
+            print("Lista despues de eliminar ", frutas)
+            break
+        else:
+            print("No se a eliminado ninguna fruta.")
+    except ValueError:
+        break
